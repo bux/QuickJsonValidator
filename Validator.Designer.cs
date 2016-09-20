cdc2ce3a2b34dc81c187c38ec339c102c33fa8e1
bux578 <github@jonathandavid.de>
2016-09-20 14:41:13 +0200

    init

---
+++ Validator.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace LargeJsonValidator {
+    partial class Validator {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(12, 12);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.ReadOnly = true;
+            this.tbPath.Size = new System.Drawing.Size(346, 20);
+            this.tbPath.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 58);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(12, 100);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblMessage.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(367, 10);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // Validator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 148);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.tbPath);
+            this.Name = "Validator";
+            this.Text = "QuickJsonValidator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnBrowse;
+    }
+}
+
---
+++ Validator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+
+
+namespace LargeJsonValidator {
+
+    public partial class Validator : Form {
+        public Validator() {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e) {
+
+            lblMessage.Text = @"Processing File ...";
+            lblMessage.ForeColor = Color.Black;
+
+            lblMessage.Refresh();
+
+            var path = tbPath.Text;
+
+            if (File.Exists(path) == false) {
+                lblMessage.Text = @"Path does not exist";
+                lblMessage.ForeColor = Color.Red;
+            } else {
+
+                using (var streamReader = new StreamReader(path)) {
+                    using (var reader = new JsonTextReader(streamReader)) {
+                        try {
+                            var schema = JSchema.Load(reader);
+
+                            lblMessage.Text = @"File seems valid.";
+                            lblMessage.ForeColor = Color.Green;
+                        } catch (JsonReaderException jrEx) {
+                            MessageBox.Show(this, jrEx.Message);
+                            lblMessage.Text = @"File is not valid.";
+                            lblMessage.ForeColor = Color.Red;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e) {
+
+            lblMessage.Text = "";
+
+            openFileDialog1.Filter = @"JSON Files|*.json";
+            openFileDialog1.FilterIndex = 1;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+                var path = openFileDialog1.FileName;
+                tbPath.Text = path;
+            }
+        }
+    }
+
+}
